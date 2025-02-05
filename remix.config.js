d786cc593e681f3028e285b14ac6ee0d582e81d2
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2025-02-05 00:53:20 -0500

    config: remix flat routes

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,20 @@
+const { flatRoutes } = require("remix-flat-routes");
+
 /** @type {import('@remix-run/dev').AppConfig} */
+
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.test.{ts,tsx}"],
   serverModuleFormat: "cjs",
+  routes: async (defineRoutes) => {
+    return flatRoutes("routes", defineRoutes, {
+      ignoredRouteFiles: ["**/.*"], // Ignore dot files (like .DS_Store)
+      //appDir: 'app',
+      //routeDir: 'routes',
+      //basePath: '/',
+      //paramPrefixChar: '$',
+      //nestedDirectoryChar: '+',
+      //routeRegex: /((\${nestedDirectoryChar}[\/\\][^\/\\:?*]+)|[\/\\]((index|route|layout|page)|(_[^\/\\:?*]+)|([^\/\\:?*]+\.route)))\.(ts|tsx|js|jsx|md|mdx)$$/,
+    });
+  },
 };
